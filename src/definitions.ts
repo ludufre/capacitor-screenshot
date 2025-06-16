6ac7d211f0bd5fd73afa01f6682ae688c2c09d84
Luan Freitas <ludufre@gmail.com>
2025-06-16 20:12:40 -0300

    fix: type definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,8 @@
 export interface ScreenshotPlugin {
+  /**
+   * Captures a screenshot of the current view.
+   * @param opts - Optional parameters for the screenshot.
+   * @returns A promise that resolves with an object containing the screenshot data.
+   */
   take(opts?: { saveToDisk?: boolean }): Promise<{ base64: string; path?: string; webPath?: string }>;
 }
